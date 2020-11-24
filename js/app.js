3dcf44d3c184d1f11d501f15cb9f31d055bd90f0
Reche-Lab <reche.dev.lab@gmail.com>
2020-11-24 11:08:03 -0300

    arrumando relógio

--- js/app.js
+++ js/app.js
@@ -15,10 +15,10 @@
     var seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
         
     // Result is output to the specific element
-    document.getElementById("days").innerHTML = days + "d "
-    document.getElementById("hours").innerHTML = hours + "h " 
-    document.getElementById("mins").innerHTML = minutes + "m " 
-    document.getElementById("secs").innerHTML = seconds + "s " 
+    document.getElementById("days").innerHTML = days + " dia "
+    document.getElementById("hours").innerHTML = hours + " h " 
+    document.getElementById("mins").innerHTML = minutes + " min " 
+    document.getElementById("secs").innerHTML = seconds + " seg " 
         
     // Display the message when countdown is over
     if (timeleft < 0) {
